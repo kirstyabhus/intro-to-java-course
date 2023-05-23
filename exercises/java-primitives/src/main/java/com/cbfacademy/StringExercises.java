d7809e18aa4ba02da5b0c8461e255d2632172f4b
kirstyabhus <kabhulimhen@gmail.com>
2023-05-23 21:44:15 +0100

    completed

--- exercises/java-primitives/src/main/java/com/cbfacademy/StringExercises.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/StringExercises.java
@@ -4,17 +4,41 @@ public class StringExercises {
 
     public String fromCharacters(char[] characters) {
         // TODO - Write code that creates a String from the input array of characters
-        throw new RuntimeException("Not implemented");
+        String word = new String(characters);
+        return word;
     }
 
     public Long howMany(String text, Character character) {
         // TODO - Write code to determine how many of the input ${character} are contained in the input ${text}
-        throw new RuntimeException("Not implemented");
+        long count = 0;
+
+        for (int i = 0; i < text.length(); i++ ) {
+            // charAt gives character at certain position
+            // check if character at position is the given character
+            if (text.charAt(i) == character) {
+                // if so then add to counter
+                ++count;
+            }
+        }
+        return count;
     }
 
     public Boolean isPalindrome(String word) {
         // TODO - Write code to determine whether the input ${word} is a palindrome
-        throw new RuntimeException("Not implemented");
+        
+        // where the reverse string will be stored
+        String reverse = "";
+        // length of word
+        int wordLength = word.length();
+        
+        // loop through every character of word string
+        for (int i = 0; i < wordLength; i++) {
+            // starting from the end of the string, add the characters into the reverse variable
+            reverse += word.charAt(wordLength - 1 - i);
+        }
+
+        // return whether reverse and word are palindrome (they should be equal if so)
+        return reverse.equals(word);
     }
 
     public String getName() {
